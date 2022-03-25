001e9c489fd6d8a8dc4cfc06876f3c3acf68bc9f
Lucien Lasseau <62194841+AlurienFlame@users.noreply.github.com>
2022-03-25 13:20:27 -0700

    Set up /record endpoint

--- index.js
+++ index.js
@@ -3,13 +3,25 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// TODO: /record endpoint to record macro
+// Eg: /record?url=https://example.com
+app.get('/record', async (req, res) => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(req.query.url);
 
+    // const client = await page.target().createCDPSession();
+    // TODO: Use chrome devtools protocol to record macro
+
+    await browser.close();
+    res.send("TODO: Deliver macro link");
+});
+
+// Eg: /replay?url=https://example.com
 app.get('/replay', async (req, res) => {
     // TODO: Read in macro data and run it with puppeteer
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://example.com');
+    await page.goto(req.query.url);
 
     const html = await page.content();
     res.send(html);
@@ -17,5 +29,5 @@ app.get('/replay', async (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`App listening on port ${port}`);
 });
